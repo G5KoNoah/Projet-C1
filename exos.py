e8d13e628eb607afab5efebb8d63e8432132d1d3
Noah <nhilaire@gaming.tech>
2022-11-08 09:37:11 +0100

    exos

--- exos.py
+++ exos.py
@@ -1,31 +1,38 @@
-def add(x,y){
+def add(x,y):
     return x+y
-}
 
-def sub(x,y){
+
+def sub(x,y):
     return x-y
-}
 
-def multiply(x,y){
+
+def multiply(x,y):
     return x*y
-}
 
-def divide(x,y){
-    return x/y
-}
 
-def modulo(x,y){
+def divide(x,y):
+    # si le diviseur est 0
+    if y == 0 :
+        # alors j'affiche une erreur
+        print("erreur de division par 0)
+        # je retourne
+        return
+    # sinon je retourne la division de x par y
+    return x / y
+
+
+def modulo(x,y):
     return x%y
-}
 
-def revenusParSec(salaireH,heureParJO,JOparAn){
+
+def revenusParSec(salaireH,heureParJO,JOparAn):
     # Je calcule le salaire annuel
     salaireAnnuel = salaireH * heureParJO * JOparAn 
     # Je calcule le nombre de seconde par an
     nbSecondeParAn = 365 * 24 * 60 * 60 
     # Je divise mon salaire par le nombre de seconde dans une année
     return salaireAnnuel / nbSecondeParAn 
-}
+
 
 def withdrawFees(total, percent):
     #calcul du montant des taxes a retirer
@@ -42,3 +49,11 @@ def calculSalaireNet(salaireBrut, public):
     else :
     # alors je retourne le salaire_brut - 23 % de taxes
         return withdrawFees(salaire_brut, 23)
+
+tour = 0
+# Tant que je ne suis pas au tour 5
+while tour != 5
+# Appeler la fonction JouerUnTour
+    jouerUnTour()
+# J'effectue l'action de passer un tour
+    tour += 1
