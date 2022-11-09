3882afcda8abef8ffc6458351c2d636c18a9fe3f
Noah <nhilaire@gaming.tech>
2022-11-09 14:44:35 +0100

    projet

---
+++ PFC.py
@@ -0,0 +1,25 @@
+
+#on admet une fonction random qui retourne un chiffre aleatoire entre 0 et 2 compris
+#on admet une fonction input qui recupere l'entree d'un joueur
+
+#on definit une fonction PFC avec comme parametre nombreJoueur et turnWin
+    #on determine un tableau avec comme valeur "pierre" , "feuille" et "ciseaux"
+    #on determine noWin a True
+    #on initialise pointJoueurA a 0
+    #on initialise pointJoueurB a 0
+    #si il n'y a qu'un joueur
+        #alors
+        #on affiche "nom du joueur :"
+        #on determine nomJoueur1 a le string de la fonction input 
+        #on determine nomJoueur2 a "IA" 
+        #tant que noWin est egal a true et que pointJoueurA est inferieur a turnWin et que pointJoueurB est inferieur a turnWin
+            #alors
+            #on initialise choixJoueurA a l'integer de la fonction input
+            #on initialise choixJoueurA a l'integer de la fonction random
+            #si choixJoueurA est égal a 0
+                #alors
+                #si choixJoueurB est égal a 1
+                    #alors 
+                    #on incremente poinJoueurA de 1
+                    #on affiche 
+
